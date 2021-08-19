72cffe5184c355ae18b2e40a5155db10e80041d2
EstherWaweru <estherwaweru86@gmail.com>
2021-08-19 18:08:28 +0300

    Chenging colours

--- chalenges/static/js/script.js
+++ chalenges/static/js/script.js
@@ -88,4 +88,63 @@ function rpsFrontEnd(yourChoice,botchoice,message){
 
 }
 
+//chalenge change color of all buttons
+ var all_buttons = document.getElementsByTagName('button');
+
+ console.log(all_buttons);
+ var copyOfButtons = [];
+for (let i=0;i<all_buttons.length;i++){
+    copyOfButtons.push(all_buttons[i].classList[1]);
+}
+console.log(copyOfButtons)
+
+function buttonColorChange(buttonattr){
+    if (buttonattr.value ==='red'){
+        buttonRed();
+    }
+    else if(buttonattr.value ==='green'){
+        buttonGreen();
+   
+    }
+    else if (buttonattr.value ==='reset'){
+        buttonReset();
+  
+    }
+    else{
+        randomColors();
+    }
+
+}
+
+function buttonRed(){
+    for(let i =0;i<all_buttons.length;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger')
+    }
+}
+function buttonGreen(){
+    for(let i =0;i<all_buttons.length;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success')
+    }
+
+}
+function buttonReset(){
+    for(let i =0;i<all_buttons.length;i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyOfButtons[i]);
+    }
+}
+
+function randomColors(){
+    var choices = ['btn-primary','btn-danger','btn-success','btn-warning'];
+    for (let i =0;i<all_buttons.length;i++){
+        var randomNumber = Math.floor(Math.random()*4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+
+
+}
+
 
